0cfda8e652af2bbf39864a1f1c63d00d07a7fa3c
andreaskhachikyan <andreaskhachikyanpolytech@gmail.com>
2024-11-20 09:39:55 +0400

    Update DoubleEncrypt.py

--- DoubleEncrypt.py
+++ DoubleEncrypt.py
@@ -1,1 +1,11 @@
-import RLE, Ceasar
+import RLE, Caesar
+
+message = input("Input message: ")
+key = int(input("Input Caesar key: "))
+
+encryptedCaesar = Caesar.Caesar.encrypt(message, key)
+print(f'Caesar Encrypted Message: {encryptedCaesar}')
+rleEncrypted = RLE.RLE.encrypt(encriptedCaesar)
+print(f'RLE encrypted message: {rleEncrypted}')
+decryptedMessage = Caesar.Caesar.decrypt(RLE.RLE.decrypt(rleEmcrypted), key)
+print(f'Decrypted message: {decryptedMessage}')
